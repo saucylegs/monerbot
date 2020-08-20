da33109b27a22b07b02df96dae553c1c974ec9cb
Noah <nsweilem@kududyn.com>
2020-08-20 13:09:35 -0700

    Some cleanup + add npm stuff

--- README.md
+++ README.md
@@ -13,3 +13,23 @@ Bot invite link: https://discord.com/api/oauth2/authorize?client_id=737476809477
 ## How the bot works
 ### Dependencies
 The bot is written in JavaScript. To run, it requires node.js with the discord.js and mysql packages. The suggestions feature requires a pre-configured MySQL database.
+
+### Selfhost quickstart
+
+1. Clone the repository
+```bash
+git clone https://github.com/saucylegs/monerbot
+cd monerbot
+```
+
+2. Install dependencies
+```bash
+npm install
+```
+
+3. Start monerbot
+```bash
+TOKEN='<discord bot token>' MYSQL_HOST='<host ip>' MYSQL_USER='<username>' MYSQL_PASSWORD='<password>' MYSQL_DATABASE='<database>' ./monerbot-github.js
+```
+
+You can omit the MySQL variables to disable the suggestions feature.
--- monerbot-github.js
+++ monerbot-github.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
-// for systemd
+ // for systemd
+
+// Start with TOKEN='<discord bot token>' MYSQL_HOST='<host ip>' MYSQL_USER='<username>' MYSQL_PASSWORD='<password>' MYSQL_DATABASE='<database>' ./monerbot-github.js
 
 'use strict';
 
@@ -9,13 +11,13 @@ const client = new Discord.Client();
 
 const regex = /^\$moner( help| count| suggest .+|)/gi;
 const suggestRegex = /^\$moner suggest /gi;
-var currency = ["moners", "yugnars", "euro", "dabloons", "dineros", "manats", "gugaloons", "gugungagayns", "sand dollars", "flongas", "shlegnats", "blorgos", "frittatas", "galindos", "deniz nakamuras", "guglamofflers", "bologiomas", "goonaras", "booraababooraas", "gigajebalos", "schinters", "scribuntos", "jugogalogs", "shanoogies", "bahoggers", "glognags", "bergelenz", "tortellinis", "bazingas", "smackaroos", "hurabarandas", "gecs", "esperantos", "csbongos", "bangalones", "furknerts", "yelnates", "rugbalooms", "gliglars", "smugars", "ungaloos", "grunkles", "kunucks", "giggleschwomps", "smegmars", "bumbershoot", "dongangonals", "quibis", "shabooyahs", "quiznos", "grongaboganls", "shumplings", "gringobringos", "gungareas"];
+var currency = require('./currencies');
 var cLength = currency.length;
 var amount = 0;
 
 client.on('ready', () => {
-    console.log("Connected as " + client.user.tag);
-    console.log("Currency count: " + cLength);
+    console.log("Connected as ", client.user.tag);
+    console.log("Currency count: ", cLength);
 })
 
 client.on('message', message => {
@@ -26,35 +28,35 @@ client.on('message', message => {
                 message.channel.send("__**MonerBot Commands**__\n`$moner` Main command. Outputs a random number with a random currency.\n`$moner help` Outputs the message you're looking at right now.\n`$moner count` Outputs the number of currencies that are currently in the bot.\n`$moner suggest CURRENCY` Use this to suggest a new currency to be added! Replace CURRENCY with your suggestion. Note that your currency will not be added right away; I have to approve them first so that the bot isn't filled with trash submissions");
                 break;
             case "$moner count":
-                message.channel.send("Currency count: " + cLength);
+                message.channel.send("Currency count: ", cLength);
                 break;
             case "$moner":
                 // Here is the actual moner part
                 // Create a 1 in 3 chance for the amount to be negative
-                var sign = Math.floor(Math.random() * 3);
-                if (sign == 0) {
-                    amount = Math.floor(Math.random() * -299) - 2;
-                } else {
-                    amount = Math.floor(Math.random() * 299) + 2;
-                }
+                var sign = Math.floor(Math.random() * 3) === 0 ? -1 : 1;
+                amount = Math.floor(Math.random() * sign * 299) - 2;
                 var cNum = Math.floor(Math.random() * cLength); // Choose an item from the array
                 var output = amount + " " + currency[cNum];
                 message.channel.send(output);
                 break;
             default:
-                if (suggestRegex.test(message.content) === true) {
+                if (suggestRegex.test(message.content) &&
+                    process.env.MYSQL_HOST &&
+                    process.env.MYSQL_USER &&
+                    process.env.MYSQL_PASSWORD &&
+                    process.env.MYSQL_DATABASE) {
                     var suggestion = message.content.replace(suggestRegex, "");
                     var author = message.author.id;
-                    
+
                     // connect to mysql database
                     var connection = mysql.createConnection({
-                        host: "REDACTED",
-                        user: "REDACTED",
-                        password: "REDACTED",
-                        database: "REDACTED"
+                        host: process.env.MYSQL_HOST,
+                        user: process.env.MYSQL_USER,
+                        password: process.env.MYSQL_PASSWORD,
+                        database: process.env.MYSQL_DATABASE
                     });
                     var sql = "INSERT INTO currencysuggest (suggestion, author, approved) VALUES (" + connection.escape(suggestion) + ", " + connection.escape(author) + ", 'no')";
-                    connection.query(sql, function (error, results, fields) {
+                    connection.query(sql, function(error, results, fields) {
                         console.log("Sent query to the database: " + sql);
                         if (error) {
                             console.log("MySQL ERROR: " + error);
@@ -62,12 +64,12 @@ client.on('message', message => {
                         } else {
                             message.channel.send("✅ Your suggestion has been sent for approval!");
                         }
-                        if (results) {console.log("Query results: " + results);}
-                        if (fields) {console.log("Info on the results fields: " + fields);}
+                        if (results) { console.log("Query results: " + results); }
+                        if (fields) { console.log("Info on the results fields: " + fields); }
                     });
                     connection.end();
                 } else {
-                    console.log("!!! Got to the switch but did not match any of the cases? Matched: " + message.content.match(regex)[0]);
+                    console.log("!!! Got to the switch but did not match any of the cases? Matched: ", message.content.match(regex)[0]);
                 }
         }
     } else {
@@ -76,4 +78,4 @@ client.on('message', message => {
 })
 
 // Use bot token from https://discord.com/developers/applications/
-client.login("REDACTED");
+client.login(process.env.TOKEN);
